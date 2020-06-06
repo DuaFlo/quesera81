aae34ce28e6a2c29e283dc03ba946d30f539bd21
Rua <juancho1000410430@gmail.com>
2020-06-05 21:08:28 -0500

    first commit

---
+++ src/quesera81/Quesera81.java
@@ -0,0 +1,16 @@
+
+package quesera81;
+
+import Vistas.Login;
+
+
+public class Quesera81 {
+
+   
+    public static void main(String[] args) {
+       Login l = new Login();
+       l.setVisible(true);//true=visible,False=invisible
+       
+    }
+    
+}
